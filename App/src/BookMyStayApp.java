26907f3dbd51b21a763691dd1ac352f9475f4c76
vishaladhityaask <vishaladhityaask11@gmail.com>
2026-03-16 15:48:53 +0530

    base completion main

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,6 +1,4 @@
 public class BookMyStayApp {
     public static void main(String[] args){
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully");
     }
 }
